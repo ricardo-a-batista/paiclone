2690218612db92ee3ecb41f233f3628947b4abe9
Ricardo Batista <iamricardobatista@gmail.com>
2024-09-22 09:51:19 +0100

    (feat): hello web, and errors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use paiclone::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = Router::new().route("/", get("Hello, Web!"));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
