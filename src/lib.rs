d8ef87116c8a24a9c69c2fd57e456bfce08abbfa
Ricardo Batista <iamricardobatista@gmail.com>
2024-09-22 10:48:13 +0100

    (chore): move home::index function to controller

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+pub mod config;
+pub mod controller;
 pub mod error;
 
+pub use crate::config::Config;
 pub use crate::error::{Error, Result};
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use axum::{routing::get, Router};
-use paiclone::Result;
+use paiclone::{controller::home, Result};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let app = Router::new().route("/", get("Hello, Web!"));
+    let app = Router::new().route("/", get(home::index));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
     axum::serve(listener, app).await?;
