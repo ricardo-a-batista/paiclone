0e46faca0b36911e4592227733a0e01afd705089
Ricardo Batista <iamricardobatista@gmail.com>
2024-09-29 20:38:23 +0100

    (feat): compression

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use axum::{routing::get, Router};
 use paiclone::{controller::home, Result};
+use tower::ServiceBuilder;
+use tower_http::{compression::CompressionLayer, decompression::RequestDecompressionLayer};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let app = Router::new().route("/", get(home::index));
+    let app = Router::new().route("/", get(home::index)).layer(
+        ServiceBuilder::new()
+            .layer(RequestDecompressionLayer::new())
+            .layer(CompressionLayer::new()),
+    );
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
     axum::serve(listener, app).await?;
