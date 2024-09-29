725f23d162c0c1cdc400f84c6790d2cf35174402
Ricardo Batista <iamricardobatista@gmail.com>
2024-09-29 20:55:16 +0100

    (feat): Static Server

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use axum::{routing::get, Router};
 use paiclone::{controller::home, Result};
 use tower::ServiceBuilder;
-use tower_http::{compression::CompressionLayer, decompression::RequestDecompressionLayer};
+use tower_http::{
+    compression::CompressionLayer, decompression::RequestDecompressionLayer, services::ServeDir,
+};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let app = Router::new().route("/", get(home::index)).layer(
-        ServiceBuilder::new()
-            .layer(RequestDecompressionLayer::new())
-            .layer(CompressionLayer::new()),
-    );
+    let app = Router::new()
+        .route("/", get(home::index))
+        .nest_service("/statics", ServeDir::new("statics"))
+        .layer(
+            ServiceBuilder::new()
+                .layer(RequestDecompressionLayer::new())
+                .layer(CompressionLayer::new()),
+        );
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
     axum::serve(listener, app).await?;
